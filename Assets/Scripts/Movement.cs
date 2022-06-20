1670631fdea0602423fd4486c0de2f3a802eb382
RafalPankowski.komp <rafal.pankowski.pks@gmail.com>
2022-06-20 17:27:19 +0200

    Added movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{ 
+    protected BoxCollider2D boxCollider;
+    protected Vector3 moveDelta;
+    protected RaycastHit2D hit;
+    public float xSpeed = 0.5f;
+    protected virtual void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    protected virtual void UpdateMotor(Vector3 input)
+    {
+        moveDelta = new Vector3(input.x * xSpeed, input.y, 0);
+        
+        transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+    }
+}
