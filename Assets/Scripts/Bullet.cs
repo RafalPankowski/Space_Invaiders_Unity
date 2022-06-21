048276554d7d439745d31fe09cc6587b453fd155
RafalPankowski.komp <rafal.pankowski.pks@gmail.com>
2022-06-21 18:41:35 +0200

    Implemented shooting by player

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    private float ySpeed = 0.75f;
+    protected Vector3 bulletMove;
+    public Transform bullet;
+
+    private void Update()
+    {
+        Shoot();
+        BulletDestroy();
+    }
+
+    void Shoot()
+    {
+     bulletMove = new Vector3(0, ySpeed, 0);
+     transform.Translate(0, bulletMove.y * Time.deltaTime, 0);
+    }
+
+    void BulletDestroy()
+    {
+        if(transform.position.y > 2.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class Player : Movement
 {
-
+    public Bullet bulletPrefarb;
+    public GameObject player;
+    private float cooldown = 0.5f;
+    private float lastShoot;
     protected override void Start()
     {
         base.Start();
@@ -14,7 +17,19 @@ public class Player : Movement
     private void FixedUpdate()
     {
         float x = Input.GetAxisRaw("Horizontal");
-
         UpdateMotor(new Vector3(x, 0, 0));
+    }
+
+    protected virtual void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (Time.time - lastShoot > cooldown)
+            {
+                lastShoot = Time.time;
+                Vector3 PBullet = new Vector3(transform.position.x, -0.55f, 0);
+                Instantiate(bulletPrefarb, PBullet, Quaternion.identity);
+            }
+        }
     }
 }
